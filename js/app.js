33b76d264e60b60b718ba96693e8c3117356e747
Ben <39630191+drachen2299@users.noreply.github.com>
2021-04-09 13:39:51 -0600

    finaly css touches and function

--- js/app.js
+++ js/app.js
@@ -2,8 +2,8 @@
                     Here is my search bar function
 -------------------------------------------------------------------------*/
 /*  1. Get the input element and place it in the searchField variable
-    3. get all of the image tags and store them in an array to use later
-    4. create a function that allows for a for loop that loops through all of the array items and 
+    3. get all of the link tags and store them in a variable to use later
+    4. create a function that allows for a for loop that loops through all of the link items and 
     store them for the function to search through
     5. use an if statement that takes the linking array, grabs the data-caption input, converts it to lowercase and then includes
     user input and then displays the a tag as an image on the screeen and filters out any images that do not include the users input.
