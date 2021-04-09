32e6f595db4d41ee2f506e7371f51c6cc9caa4b1
Ben <39630191+drachen2299@users.noreply.github.com>
2021-04-09 13:15:44 -0600

    refactored search function and added bottom shadow

--- js/app.js
+++ js/app.js
@@ -1,27 +1,30 @@
-/*-----------------------------------------------
-This is my search bar function
-------------------------------------------------*/
-//select the input element
-const input = document.getElementById('search');
+/*------------------------------------------------------------------------
+                    Here is my search bar function
+-------------------------------------------------------------------------*/
+/*  1. Get the input element and place it in the searchField variable
+    3. get all of the image tags and store them in an array to use later
+    4. create a function that allows for a for loop that loops through all of the array items and 
+    store them for the function to search through
+    5. use an if statement that takes the linking array, grabs the data-caption input, converts it to lowercase and then includes
+    user input and then displays the a tag as an image on the screeen and filters out any images that do not include the users input.
+*/
 
-//create the function that handles the search and filtering
-const getSearch = (e) => {
-    let inputValue = e.target.value.toLowerCase();
-    let array = document.getElementsByClassName('item');
+//Declare the variables to be used
+const searchField = document.getElementById("search");
+const linking = document.getElementsByTagName("a");
 
-    for (let i = 0; i < array.length; i++) {
-        let image = array[i];
-        let caption = image.getAttribute('data-caption').toLowerCase();
-        let title = image.getAttribute('title').toLowerCase();
-        //truthy falsy short hand if statement that will display the image or not. 
-        caption.includes(inputValue) || title.includes(inputValue) ? image.style.display = 'inline' : image.style.display = 'none';
+//Create the searchfield function that listens for key presses
+searchField.addEventListener('keyup', function(event) {
+    const b = event.target.value.toLowerCase();
+    //use a for loop to create the array
+    for (let i = 0; i < linking.length; i++) {
+        //use an if statement that filters the images not included in user searches
+        if (linking[i].getAttribute("data-caption").toLowerCase().includes(b)){
+            linking[i].style.display = '';
+        }else {
+            linking[i].style.display = 'none';
+        }
 
     }
-}
-input.addEventListener('keyup', getSearch);
-
-baguetteBox.run('.gallery', {
-    captions: true,
-    buttons: 'auto',
-    animation: 'fadeIn'
 });
+
